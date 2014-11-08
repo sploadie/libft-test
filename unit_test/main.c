5596d8594f2b7310296d6e21e5fb70d1ecb59f7e
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2014-11-08 15:29:16 +0100

    Fixed multiple tests, added additional post-bonus function tests

--- unit_test/main.c
+++ unit_test/main.c
@@ -18,6 +18,12 @@
 
 /* #define NO_BONUS */
 
+/*
+** If you want to try making fuctions /after/ the bonuses :
+*/
+
+/* #define SPL_EXTRA */
+
 UT_TEST(ft_memset)
 {
 	char	b1[100], b2[100];
@@ -36,7 +42,7 @@ UT_TEST(ft_bzero)
 	char	b1[10], b2[10];
 
 	bzero(b1, 10);
-	bzero(b2, 10);
+	ft_bzero(b2, 10);
 	UT_ASSERT_EQ(memcmp(b1, b2, 10), 0);
 	b1[0] = 1;
 	ft_bzero(b1, 0);
@@ -47,6 +53,8 @@ UT_TEST(ft_memcpy)
 {
 	char	b1[100], b2[100];
 
+	memset(b1, 33, 100);
+	memset(b2, 63, 100);
 	ft_memcpy(b1, b2, 100);
 	UT_ASSERT_EQ(memcmp(b1, b2, 100), 0);
 	UT_ASSERT_EQ(b1, ft_memcpy(b1, b2, 0));
@@ -94,12 +102,14 @@ UT_TEST(ft_memcmp)
 	UT_ASSERT_NEQ(ft_memcmp(t, "abcab", 5) ,0);
 	UT_ASSERT(ft_memcmp("aaa", "aab", 4) < 0);
 	UT_ASSERT_EQ(ft_memcmp("aww", "bpp", 0), 0);
+	UT_ASSERT(ft_memcmp("\200", "\0", 1) > 0);
 }
 
 UT_TEST(ft_strlen)
 {
 	UT_ASSERT_EQ(ft_strlen("chat"), strlen("chat"));
 	UT_ASSERT_EQ(ft_strlen(""), strlen(""));
+	UT_ASSERT_EQ(ft_strlen("aaa\0aaa"), strlen("aaa\0aaa"));
 }
 
 UT_TEST(ft_strdup)
@@ -116,18 +126,22 @@ UT_TEST(ft_strcpy)
 {
 	char	buf[6];
 
+	bzero(buf, 6);
 	ft_strcpy(buf, "abcde");
 	UT_ASSERT_EQ(strcmp(buf, "abcde"), 0);
+	ft_strcpy(buf, "abc");
+	UT_ASSERT_EQ(strcmp(buf, "abc"), 0);
 }
 
 UT_TEST(ft_strncpy)
 {
-	char	b[6];
+	char	buf[6];
 
-	ft_strncpy(b, "abc", 6);
-	UT_ASSERT_EQ(memcmp(b, "abc\0\0", 6), 0);
-	ft_strncpy(b, "abcdefghi", 6);
-	UT_ASSERT_EQ(memcmp(b, "abcdefghi", 6), 0);
+	memset(buf, 33, 6);
+	ft_strncpy(buf, "abc", 6);
+	UT_ASSERT_EQ(memcmp(buf, "abc\0\0", 6), 0);
+	ft_strncpy(buf, "abcdefghi", 6);
+	UT_ASSERT_EQ(memcmp(buf, "abcdefghi", 6), 0);
 }
 
 UT_TEST(ft_strcat)
@@ -135,8 +149,10 @@ UT_TEST(ft_strcat)
 	char	buf[9];
 
 	bzero(buf, 9);
+	ft_strcat(buf, "");
 	ft_strcat(buf, "Bon");
-	ft_strcat(buf, "jour.");
+	ft_strcat(buf, "j");
+	ft_strcat(buf, "our.");
 	ft_strcat(buf, "");
 	UT_ASSERT_EQ(strcmp(buf, "Bonjour."), 0);
 	UT_ASSERT_EQ(buf, ft_strcat(buf, ""));
@@ -197,6 +213,8 @@ UT_TEST(ft_strstr)
 	char	buf[] = "Ceci n'est pas une pipe.";
 
 	UT_ASSERT_EQ(strstr(buf, "une"), ft_strstr(buf, "une"));
+	UT_ASSERT_EQ(strstr(buf, "C"), ft_strstr(buf, "C"));
+	UT_ASSERT_EQ(strstr(buf, "."), ft_strstr(buf, "."));
 	UT_ASSERT_EQ(strstr(buf, ""), ft_strstr(buf, ""));
 	UT_ASSERT_EQ(strstr(buf, "aaaaa"), ft_strstr(buf, "aaaaa"));
 	UT_ASSERT_EQ(ft_strstr(buf, "BWAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), NULL);
@@ -400,7 +418,7 @@ UT_TEST(ft_strmap)
 
 	str = "abcde";
 	str = ft_strmap(str, map_test);
-	UT_ASSERT_EQ(ft_strcmp(str, "bcdef"), 0);
+	UT_ASSERT_EQ(strcmp(str, "bcdef"), 0);
 }
 
 char	mapi_test(unsigned int i, char c)
@@ -414,7 +432,7 @@ UT_TEST(ft_strmapi)
 
 	str = "abcde";
 	str = ft_strmapi(str, mapi_test);
-	UT_ASSERT_EQ(ft_strcmp(str, "acegi"), 0);
+	UT_ASSERT_EQ(strcmp(str, "acegi"), 0);
 }
 
 UT_TEST(ft_strequ)
@@ -739,6 +757,91 @@ UT_TEST(ft_lstmap)
 
 #endif
 
+#ifdef SPL_EXTRA
+
+UT_TEST(ft_memdup)
+{
+	char	*c;
+
+	c = "AbC";
+	UT_ASSERT_EQ(strcmp(ft_memdup("aaaaa", 6), "aaaaa"), 0);
+	UT_ASSERT_EQ(strcmp(ft_memdup("", 1), ""), 0);
+	UT_ASSERT_NEQ(c, ft_memdup(c, 4));
+}
+
+UT_TEST(ft_strrev)
+{
+	char	buf1[] = "123456";
+	char	buf2[] = "12345";
+	char	buf3[] = "1";
+	char	buf4[] = "";
+
+	UT_ASSERT_EQ(strcmp(ft_strrev(buf1), "654321"), 0);
+	UT_ASSERT_EQ(strcmp(ft_strrev(buf2), "54321"), 0);
+	UT_ASSERT_EQ(strcmp(ft_strrev(buf3), "1"), 0);
+	UT_ASSERT_EQ(strcmp(ft_strrev(buf4), ""), 0);
+}
+
+UT_TEST(ft_ptrswap)
+{
+	int		was_1 = 1;
+	int		was_2 = 2;
+	char	was_a = 'a';
+	char	was_b = 'b';
+	int		*ptr_1 = &was_1;
+	int		*ptr_2 = &was_2;
+	char	*ptr_a = &was_a;
+	char	*ptr_b = &was_b;
+
+	ft_ptrswap(&ptr_1, &ptr_2);
+	ft_ptrswap(&ptr_a, &ptr_b);
+	UT_ASSERT(*ptr_1 == 2);
+	UT_ASSERT(*ptr_2 == 1);
+	UT_ASSERT(*ptr_a == 'b');
+	UT_ASSERT(*ptr_b == 'a');
+}
+
+UT_TEST(ft_memswap)
+{
+	char	was_a = 'a';
+	char	was_b = 'b';
+	int		was_1 = 1;
+	int		was_2 = 2;
+	char	was_toto[] = "toto";
+	char	was_tata[] = "tata";
+
+	ft_memswap(&was_a, &was_b, sizeof(char));
+	UT_ASSERT(was_a == 'b');
+	UT_ASSERT(was_b == 'a');
+	ft_memswap(&was_1, &was_2, sizeof(int));
+	UT_ASSERT(was_1 == 2);
+	UT_ASSERT(was_2 == 1);
+	ft_memswap(&was_1, &was_2, 0);
+	UT_ASSERT(was_1 == 2);
+	UT_ASSERT(was_2 == 1);
+	ft_memswap(was_toto, was_tata, sizeof(char) * 2);
+	UT_ASSERT_EQ(strcmp(was_toto, "tato"), 0);
+	UT_ASSERT_EQ(strcmp(was_tata, "tota"), 0);
+}
+
+UT_TEST(ft_strlcpy)
+{
+	char	buf[10];
+	size_t	output;
+
+	output = ft_strlcpy(buf, "1234567890", 10);
+	UT_ASSERT_EQ(strcmp(buf, "123456789"), 0);
+	UT_ASSERT_EQ(output, 10);
+	output = ft_strlcpy(buf, "12345", 10);
+	UT_ASSERT_EQ(strcmp(buf, "12345"), 0);
+	UT_ASSERT_EQ(output, 5);
+	output = ft_strlcpy(buf, "1234567890", 0);
+	UT_ASSERT_EQ(strcmp(buf, "12345"), 0);
+	UT_ASSERT_EQ(output, 10);
+}
+
+#endif
+
 int	main(void)
 {
 	UT_ADD_TEST(ft_memset);
@@ -803,7 +906,13 @@ int	main(void)
 	UT_ADD_TEST(ft_lstiter);
 	UT_ADD_TEST(ft_lstmap);
 #endif
+#ifdef	SPL_EXTRA
+	UT_ADD_TEST(ft_memdup);
+	UT_ADD_TEST(ft_strrev);
+	UT_ADD_TEST(ft_ptrswap);
+	UT_ADD_TEST(ft_memswap);
+	UT_ADD_TEST(ft_strlcpy);
+#endif
 	UT_RUN_ALL_TESTS();
 	return (0);
 }
-
