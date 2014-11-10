26b5e142726ac3f46833f49a062dacfa05e31a0f
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2014-11-10 19:07:00 +0100

    Added a missed memcmp test.

--- unit_test/main.c
+++ unit_test/main.c
@@ -102,6 +102,7 @@ UT_TEST(ft_memcmp)
 	UT_ASSERT_EQ(ft_memcmp(t, t, 5), 0);
 	UT_ASSERT_NEQ(ft_memcmp(t, "abcab", 5) ,0);
 	UT_ASSERT(ft_memcmp("aaa", "aab", 4) < 0);
+	UT_ASSERT_EQ(ft_memcmp("aab", "aac", 2), 0);
 	UT_ASSERT_EQ(ft_memcmp("aww", "bpp", 0), 0);
 	UT_ASSERT(ft_memcmp("\200", "\0", 1) > 0);
 }
